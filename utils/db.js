fc3320208ae298a4cbf7f1512cb38f5cdce8a333
saitesaraficode10 <newwaypayteam@gmail.com>
2026-09-15 18:20:34 +0400

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,13 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 
-const dbPath = path.join(__dirname, '..', 'db', 'sarrafi.db');
+const dbDir = path.join(__dirname, '..', 'db');
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir, { recursive: true });
+}
+
+const dbPath = path.join(dbDir, 'sarrafi.db');
 const db = new Database(dbPath);
 
 db.pragma('journal_mode = WAL');
